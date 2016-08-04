5ee3fdd11ad99efaa48bad9f458743b420f26fdf
rongbin.wei <rongbin.wei@wenba100.com>
2016-08-04 18:37:21 +0800

    add a new file

---
+++ dota2_data_analyse/dota2_steam_api.py
@@ -0,0 +1,6 @@
+
+from steam import SteamClient
+from dota2 import Dota2Client
+import logging
+
+logging.basicConfig(format='[%(asctime)s] %(levelname)s %(name)s: %(message)s', level=logging.DEBUG)
