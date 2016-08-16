0bb319d2c4ea68eed3dfebf72ee23d3c3d541dd6
weirongbin <1243764818@qq.com>
2016-08-16 23:36:49 +0800

    update

--- dota2_data_analyse/douyu_danmu.py
+++ dota2_data_analyse/douyu_danmu.py
@@ -13,10 +13,177 @@ import http.cookiejar
 import json
 import ssl
 import os
+import struct
+import codecs
+import random
 
-class LoginClient:
+class DouyuApi:
+
     '''
-    Used to login douyu and get some login info
+    Some api to get info about douyu
+    ref: http://430.io/-xie-dou-yu-tv-web-api-some-douyutv-api/
+    '''
+
+    __douyu_api_host = 'http://capi.douyucdn.cn/api/v1/'
+
+    def __init__(self, user_name, password):
+        self.__user_name = user_name
+        self.__password = password
+        self.__login_info = None
+
+    def __login(self):
+        
+        password_md5 = hashlib.md5(self.__password.encode('utf-8')).hexdigest()
+        params = urllib.parse.urlencode({'username' : self.__user_name,
+                                  'password' : password_md5
+                                  })
+        url = DouyuApi.__douyu_api_host + 'login?' + params
+        result = urllib.request.urlopen(url).read().decode()
+        result_dict = json.loads(result)
+        if result_dict['error'] is 0:
+            self.__login_info = result_dict
+
+    def get_lives(self, count = -1, parent_cate_id = None, child_cate_id = None):
+        '''
+        Get the lives, if count is -1, get all the lives
+        '''
+        url_prefix = str()
+        if child_cate_id is not None:
+            url_prefix = DouyuApi.__douyu_api_host + 'live/' + child_cate_id + '?offset='
+        elif parent_cate_id is not None:
+            url_prefix = DouyuApi.__douyu_api_host + 'getColumnRoom/' + parent_cate_id + '?offset='
+        else:
+            url_prefix = DouyuApi.__douyu_api_host + 'live/' + '?offset='
+
+        if count is -1:
+            left_count = os.sys.maxsize
+        else:
+            left_count = count
+
+        room_list = []
+        offset = 0
+        while left_count > 0:
+            url = url_prefix + str(offset)
+            result = urllib.request.urlopen(url).read().decode()
+            room_info_dict = json.loads(result)
+            if room_info_dict['error'] == 0:
+                cur_room_list = room_info_dict['data']
+                if len(cur_room_list) == 0:
+                    break
+                for room_info in cur_room_list:
+                    room_list.append(room_info)
+                    left_count -= 1
+                    if left_count == 0:
+                        break
+                print(len(room_list))
+                offset += len(cur_room_list)
+            else:
+                break
+        return room_list
+
+    def get_parent_cate_list(self):
+        url = DouyuApi.__douyu_api_host + 'getColumnList'
+        result = urllib.request.urlopen(url).read().decode()
+        result_dict = json.loads(result)
+        if result_dict['error'] == 0:
+            parent_cate_list = result_dict['data']
+            return parent_cate_list
+
+    def get_child_cate_list(self, parent_short_name):
+        url = DouyuApi.__douyu_api_host + 'getColumnDetail?shortName=' + parent_short_name
+        result = urllib.request.urlopen(url).read().decode()
+        result_dict = json.loads(result)
+        if result_dict['error'] == 0:
+            child_cate_list = result_dict['data']
+            return child_cate_list
+
+    def search_live(self, search_content):
+        url = DouyuApi.__douyu_api_host + 'searchNew/' + search_content + '/1?'
+        result = urllib.request.urlopen(url).read().decode()
+        result_dict = json.loads(result)
+        if result_dict['error'] == 0:
+            search_list = result_dict['data']
+            return search_list
+
+    def get_self_info(self):
+        if not self.__login_info:
+            self.__login()
+            return self.__login_info
+
+    def get_room_info(self, room_id):
+        auth_str = '79420'
+        auth_md5 = hashlib.md5(auth_str.encode('utf-8')).hexdigest()
+        time_str = str(int(time.time()))
+        auth_str = 'room/' + room_id + '?aid=android&client_sys=android&ne=1&support_pwd=0&time=' + time_str
+        auth_md5 = hashlib.md5(auth_str.encode('utf-8')).hexdigest()
+        params = '?aid=android&client_sys=android&ne=1&support_pwd=1&time=' + time_str
+        #urllib.parse.urlencode({'aid' : 'android',
+        #                                 'client_sys' : 'android',
+        #                                 'time' : time_str,
+        #                                 'auth' : auth_md5})
+        url = DouyuApi.__douyu_api_host +  'room/' + room_id + params
+        result = urllib.request.urlopen(url).read().decode()
+        result_dict = json.loads(result)
+        if result_dict['error'] == 0:
+            return result['data']
+
+    def get_follow_room_list(self):
+        if not self.__login_info:
+            self.__login()
+        if self.__login_info:
+            follow_room_list = []
+            token = self.__login_info['data']['token']
+            params = urllib.parse.urlencode({'token' : token})
+            url = DouyuApi.__douyu_api_host + 'followRoom?live=0&' + params
+            result = urllib.request.urlopen(url).read().decode()
+            result_dict = json.loads(result)
+            if result_dict['error'] is 0:
+                for follow_room in result_dict['data']:
+                    follow_room_list.append(follow_room)
+            
+            url = DouyuApi.__douyu_api_host + 'followRoom?live=1&' + params
+            result = urllib.request.urlopen(url).read().decode()
+            result_dict = json.loads(result)
+            if result_dict['error'] is 0:
+                for follow_room in result_dict['data']:
+                    follow_room_list.append(follow_room)
+
+            if len(follow_room_list) > 0:
+                return follow_room_list
+
+    def del_follow(self, *ids):
+        if not self.__login_info:
+            self.__login()
+        if self.__login_info:
+            token = self.__login_info['data']['token']
+            params = urllib.parse.urlencode({'token' : token})
+            url = DouyuApi.__douyu_api_host + 'follow/del?' + params
+            post_data = 'ids:'
+            for id in ids:
+                post_data += id + ','
+            post_data = post_data.strip(',')
+
+            result = urllib.request.urlopen(url, data = post_data.encode('utf-8')).read().decode()
+            result_dic = json.loads(result)
+            print(result)
+
+    def get_view_history(self):
+        if not self.__login_info:
+            self.__login()
+        if self.__login_info:
+            token = self.__login_info['data']['token']
+            params = urllib.parse.urlencode({'token' : token})
+            url = DouyuApi.__douyu_api_host + 'history?' + params
+            result = urllib.request.urlopen(url).read().decode()
+            result_dict = json.loads(result)
+            if result_dict['error'] is 0:
+                view_history = result_dict['data']
+                return view_history
+
+
+class WebLoginClient:
+    '''
+    Used to login douyu though the webpage and get some login info
     '''
     __headers = { 'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.87 Safari/537.36'}
     __douyu_cookie_file = 'douyu_cookie'
@@ -28,11 +195,11 @@ class LoginClient:
         self.__password = password
         self.__login_info = None
         self.__room_info_dict = {}
-        if os.path.exists(LoginClient.__douyu_cookie_file):
+        if os.path.exists(WebLoginClient.__douyu_cookie_file):
             self.__cookie_jar = http.cookiejar.MozillaCookieJar()
-            self.__cookie_jar.load(LoginClient.__douyu_cookie_file)
+            self.__cookie_jar.load(WebLoginClient.__douyu_cookie_file)
         else:
-            self.__cookie_jar = http.cookiejar.MozillaCookieJar(LoginClient.__douyu_cookie_file)
+            self.__cookie_jar = http.cookiejar.MozillaCookieJar(WebLoginClient.__douyu_cookie_file)
 
         self.__https_opener = self.__get_https_opener()
 
@@ -58,7 +225,7 @@ class LoginClient:
 
     def __is_login_cookie_valid(self):
         # Test if the saved cookies is valid
-        profile_url = LoginClient.__douyu_host_url + 'member'
+        profile_url = WebLoginClient.__douyu_host_url + 'member'
         response = self.__http_opener.open(profile_url).read().decode()
         match = re.search('<title>(.*?)</title>', response)
         if match and '个人中心' in match.group(1):
@@ -67,8 +234,8 @@ class LoginClient:
 
     def __login_douyu_real(self):
         captcha_id = str(int(time.time() * 1000))
-        captch_url = LoginClient.__passport_douyu_host_url + 'api/captcha?v=' + captcha_id
-        captcha_request = urllib.request.Request(captch_url, headers = LoginClient.__headers)
+        captch_url = WebLoginClient.__passport_douyu_host_url + 'api/captcha?v=' + captcha_id
+        captcha_request = urllib.request.Request(captch_url, headers = WebLoginClient.__headers)
         response = self.__https_opener.open(captcha_request)
         captcha_code = input('captcha code:')
 
@@ -80,9 +247,9 @@ class LoginClient:
                                 'captcha_word': captcha_code,
                                 't': int(time.time() * 1000)
                                 })
-        login_url = LoginClient.__passport_douyu_host_url + 'iframe/login?' + params
+        login_url = WebLoginClient.__passport_douyu_host_url + 'iframe/login?' + params
     
-        login_request = urllib.request.Request(login_url, headers = LoginClient.__headers)
+        login_request = urllib.request.Request(login_url, headers = WebLoginClient.__headers)
         response = self.__https_opener.open(login_request).read().decode()
         json_resonse = response.strip('()')
         response_dict = json.loads(json_resonse)
@@ -99,8 +266,8 @@ class LoginClient:
                                                 'client_id': 1
                                             })
 
-            auth_url = LoginClient.__douyu_host_url + 'api/passport/login?' + params
-            auth_request = urllib.request.Request(auth_url, headers= LoginClient.__headers)
+            auth_url = WebLoginClient.__douyu_host_url + 'api/passport/login?' + params
+            auth_request = urllib.request.Request(auth_url, headers= WebLoginClient.__headers)
             response = self.__https_opener.open(auth_request).read().decode()
             return True
 
@@ -127,16 +294,19 @@ class LoginClient:
 
         room_info = {}
 
-        url = LoginClient.__douyu_host_url + room_id
+        url = WebLoginClient.__douyu_host_url + room_id
         html = self.__http_opener.open(url).read().decode()
 
         match = re.search('var\s\$ROOM\s=\s({.*});', html)
         if match != None:
             room_info_json = match.group(1)
-            room_info_dict = json.loads(room_info_json)
-            room_info['room_info'] = room_info_dict
+            try:
+                room_info_dict = json.loads(room_info_json)
+                room_info['room_info'] = room_info_dict
+            except ValueError as e:
+                print(e)
 
-        match = re.search('\$ROOM\.args\s=\s({.*});', html)
+        match = re.search('var\sroom_args\s=\s({.*});', html)
         if match != None:
             auth_server_json = match.group(1)
             auth_server_dict = json.loads(auth_server_json)
@@ -168,7 +338,7 @@ class DanmuClient:
     __address_danmu_4 = ('danmu.douyutv.com', 12602)
 
     def __init__(self, user_name, password, room_id):
-        self.__douyu_login_client = LoginClient(user_name, password)
+        self.__douyu_login_client = WebLoginClient(user_name, password)
         self.__user_name = user_name
         self.__password = password
         self.__room_id = room_id
@@ -209,11 +379,12 @@ class DanmuClient:
             print(e)
 
     def __wrap_danmu_msg(self, content):
-        length = bytearray([len(content) + 10, 0x00, 0x00, 0x00])
+        content_bytes = content.encode('utf-8')
+        length =struct.pack('<i', len(content_bytes) + 10)
         code = length
         magic = bytearray([0xb1, 0x02, 0x00, 0x00])
         end = b'/\x00'
-        return length + code + magic + bytes(content.encode('utf-8')) + end
+        return length + code + magic + content_bytes + end
 
     def __send_msg(self, msg, socket):
         data = self.__wrap_danmu_msg(msg)
@@ -234,9 +405,18 @@ class DanmuClient:
     def __auth_socket_keep_alive(self):
         while True:
             time_str = str(int(time.time()))
-            data = 'type@=keeplive/tick@='+ time_str
+            time_md5 = hashlib.md5(time_str.encode('utf-8')).hexdigest()
+            data = 'type@=keeplive/tick@='+ time_str + '/vbw@=0/k@=' + time_md5
             self.__send_msg(data, self.__auth_socket)
             time.sleep(45)
+
+    def __auth_socket_recv(self):
+        file = codecs.open('temp.txt', mode='w', encoding='utf-8')
+        while True:
+            data = self.__get_next_data(self.__auth_socket)
+            if data:
+                file.write(data + os.linesep)
+                file.flush()
 
     def __recv_socket_keep_alive(self):
         while True:
@@ -257,7 +437,9 @@ class DanmuClient:
         if 'auth_server_list' not in self.__room_info:
             print('Get auth server failed')
 
-        auth_server = (self.__room_info['auth_server_list'][0]['ip'], int(self.__room_info['auth_server_list'][0]['port']))
+        index = int(random.random() * len(self.__room_info['auth_server_list']))
+
+        auth_server = (self.__room_info['auth_server_list'][index]['ip'], int(self.__room_info['auth_server_list'][index]['port']))
 
         self.__auth_socket.connect(auth_server)
 
@@ -266,7 +448,7 @@ class DanmuClient:
         vk_md5_str = hashlib.md5(bytes(time_str + '7oE9nPEG9xXV69phU31FYCLUagKeYtsF' + login_info['acf_devid'], 'utf-8')).hexdigest()
         password_md5 = hashlib.md5(bytes(self.__password, 'utf-8')).hexdigest()
         login_data = 'type@=loginreq/username@=' + login_info['acf_username'] + '/ct@=' + login_info['acf_ct'] +\
-                        '/password@=' + '/roomid@=' + self.__room_id + '/devid@=' + login_info['acf_devid'] + '/rt@=' + time_str +\
+                        '/password@=' + password_md5 + '/roomid@=' + self.__room_id + '/devid@=' + login_info['acf_devid'] + '/rt@=' + time_str +\
                         '/vk@=' + vk_md5_str + '/ver@=20150929/aver@=2016080605' + '/ltkid@=' + login_info['acf_ltkid'] +\
                         '/biz@=' + login_info['acf_biz'] + '/stk@=' + login_info['acf_stk']
         self.__send_msg(login_data, self.__auth_socket)
@@ -278,7 +460,21 @@ class DanmuClient:
             self.__uid = parse_result['userid']
             self.__sessionid = parse_result['sessionid']
    
+            # Send qrl msg
+            content = 'type@=qrl/rid@=' + self.__room_id + '/et@=0'
+            self.__send_msg(content, self.__auth_socket)
+            # Send qtlnq msg
+            content = 'type@=qtlnq'
+            self.__send_msg(content, self.__auth_socket)
+            # Send qtlq msg
+            content = 'type@=qtlq'
+            self.__send_msg(content, self.__auth_socket)
+            # Send reqog msg
+            content = 'type@=reqog/uid@=' + self.__uid
+            self.__send_msg(content, self.__auth_socket)
+
             recv_data_str = self.__get_next_data(self.__auth_socket)
+            print(recv_data_str)
             while 'type@=setmsggroup' not in recv_data_str:
                 recv_data_str = self.__get_next_data(self.__auth_socket)
 
@@ -286,6 +482,7 @@ class DanmuClient:
             self.__gid = parse_result['gid']
 
             threading._start_new_thread(self.__auth_socket_keep_alive, ())
+            threading._start_new_thread(self.__auth_socket_recv, ())
             return True
 
         return False
@@ -407,15 +604,50 @@ class DanmuClient:
 
 if __name__ == '__main__':
     room_id = '97376'
-    user_name = input('user name:')
-    password = input('password:')
 
-    danmu_client = DanmuClient(user_name, password, room_id)
-    result = danmu_client.login_danmu_server(True)
+
+    user_name = 'xiaaowei'#input('user name:')
+    password = 'chpeui1990'#input('password:')
+
+    douyu_api = DouyuApi(user_name, password)
+    douyu_api.get_room_info(room_id)
+    parent_cate_list = douyu_api.get_parent_cate_list()
+    child_cate_list = douyu_api.get_child_cate_list('game')
+
+    live_list = douyu_api.get_lives(child_cate_id = '1')
+    ##live_list = douyu_api.get_lives(parent_cate_id = '2')
+    #live_list = douyu_api.get_lives(count=10, child_cate_id = '3')
+    #live_list = douyu_api.get_lives(9)
+
+    #self_info = douyu_api.get_self_info()
+    #follow_room_list = douyu_api.get_follow_room_list()
+
+    #douyu_api.del_follow(follow_room_list[0]['room_id'])
+    #view_room_history = douyu_api.get_view_history()
+
+    #douyu_api.search_live('yyf')
+
+    danmu_client_list = []
+
+    for live in live_list:
+        danmu_client = DanmuClient(user_name, password, live['room_id'])
+        if danmu_client.login_danmu_auth_server():
+            danmu_client_list.append(danmu_client)
+        else:
+            print(live['room_id'])
+
     while True:
-        #msg = input('message:')
-        #danmu_client.send_one_danmu(msg)
-        danmu = danmu_client.get_one_danmu()
-        if danmu:
-            danmu_client.parse_danmu(danmu)
+        msg = input('message:')
+        for danmu_client in danmu_client_list:
+            danmu_client.send_one_danmu(msg)
+
+
+    #danmu_client = DanmuClient(user_name, password, room_id)
+    #result = danmu_client.login_danmu_server(True)
+    #while True:
+    #    msg = input('message:')
+    #    danmu_client.send_one_danmu(msg)
+        #danmu = danmu_client.get_one_danmu()
+        #if danmu: 
+        #    danmu_client.parse_danmu(danmu)
 
